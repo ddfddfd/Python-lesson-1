1164ef13de6b381c697d293831c11c88de6bf2e4
Muhammad bin Adeel <funwithyo123@gmail.com>
2026-03-01 17:54:19 +0500

    Intro to dictionary

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+#Variables store single value 
+name = "muhammad bin adeel."
+print(name )
+#LiSTS can store multiple values togather
+names=["muhammad","anaya"]
+print(names)
+#dictionarys can store values and pairs
+student={"anaya":1,
+         "muhammad":100}
+print(student)
+#dictionaries stores values in key:value pairs
+#left of : is key
+#right of : is value
+#key stores the value
+print(student.keys())
+print(student.values())
+#add an item to dictionary
+student["hassan"]=99
+print(student)
